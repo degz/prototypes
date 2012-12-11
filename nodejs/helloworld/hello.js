482ef12f3588d3040b1fdf84c159674cd69f34dc
Sudeep Vishnumurthy <sudeepaithal@yahoo.com>
2012-12-11 01:49:48 -0500

    simple oo js

--- nodejs/helloworld/hello.js
+++ nodejs/helloworld/hello.js
@@ -1,9 +1,22 @@
 // reference the http module so we can create a webserver
 var http = require("http");
+var pr = require("./parserequest.js");
+var li = require("./li.js");
+
+var ctr = 1;
 
 // create a server
 http.createServer(function(req, res) {
-    console.log("Hello world!");
-    res.end("Hello world!");
+    var userreq = pr.process(req);
+    var mesg = 'Hello World \n';
 
+    if(userreq.app == 'li') {
+        li.process(userreq.op);
+    }
+
+    console.log(mesg);
+    console.log(ctr++);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(mesg);
 }).listen(process.env.PORT, process.env.IP);
